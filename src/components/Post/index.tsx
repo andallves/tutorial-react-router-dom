7a2f09e1d92a70102cfc5ee5a702ad654e77ad77
Andre Alves <andrealves10a@gmail.com>
2023-05-20 10:51:44 -0300

    Initial

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export const Post = () => {
+  return (
+    <div>
+      <h1>I'm componenet CardPost</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda delectus itaque similique iure incidunt maxime voluptatibus dolore nesciunt mollitia velit? Delectus et odit rerum porro provident perferendis magnam distinctio quibusdam.
+      </p>
+    </div>
+  );
+};
